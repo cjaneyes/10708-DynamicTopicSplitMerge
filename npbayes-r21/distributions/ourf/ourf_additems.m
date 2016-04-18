b0ed919e2610d27d16b81b293c97dc06c9565717
lxuad <lxuad.xlxiao28@gmail.com>
2016-04-18 13:32:03 -0400

    correct matlab file (additems)

--- npbayes-r21/distributions/ourf/ourf_additems.m
+++ npbayes-r21/distributions/ourf/ourf_additems.m
@@ -1,5 +1,16 @@
-function qq = Multinomial_additems(hh,qq,cc,ss);
+function qq = ourf_additems(hh,qq,cc,ss);
 
-sc = sparse(ss,cc,ones(size(ss)),size(qq,1),size(qq,2));
-qq = qq + sc;
+%sc = sparse(ss,cc,ones(size(ss)),size(qq,1),size(qq,2));
+%qq = qq + sc;
+
+if numel(ss) == 0
+    return
+end
+
+[W, D] = size(ss);
+row = 1:D;
+for ii=1:W
+    sc = sparse(row, cc, ss(ii,:), size(qq,1), size(qq,2));
+    qq = qq + sc;
+end
 
