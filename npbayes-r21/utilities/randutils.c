a4f23f81cea764a859eb3aab5e385e5e48142d2e
lxuad <lxuad.xlxiao28@gmail.com>
2016-04-18 21:51:19 -0400

    commit log_nomalize, etc

--- npbayes-r21/utilities/randutils.c
+++ npbayes-r21/utilities/randutils.c
@@ -24,6 +24,71 @@ double drand48()
 
 #endif
 
+double max_element(const double * x, int n, int* argmax)
+{
+    *argmax = 0;
+    double max_val = x[0];
+    for (int i = 1; i < n; i++)
+        if (x[i] > max_val)
+        {
+            max_val = x[i];
+            *argmax = i;
+        }
+    return max_val;
+}
+
+/**
+ * given log(a) and log(b), return log(a + b)
+ *
+ */
+
+double log_sum(double log_a, double log_b)
+{
+    double v;
+
+    if (log_a < log_b)
+        v = log_b+log(1 + exp(log_a-log_b));
+    else
+        v = log_a+log(1 + exp(log_b-log_a));
+    return v;
+}
+
+
+// give a_1, ..., a_n,
+// return log(exp(a_1)+...+exp(a_n))
+double log_normalize(double * array, int nlen)
+{
+   const double log_max = 100.0; // the log(maximum in double precision), make sure it is large enough.
+   int argmax;
+   double max_val = max_element(array, nlen, &argmax); //get the maximum value in the array to avoid overflow
+   double log_shift = log_max - log(nlen + 1.0) - max_val;
+   double sum = 0.0;
+   for (int i = 0; i < nlen; i++)
+       sum += exp(array[i] + log_shift); //shift it
+
+   double log_norm = log(sum) - log_shift;
+   for (int i = 0; i < nlen; i++)
+       array[i] -= log_norm; //shift it back
+
+   return log_norm;
+}
+
+
+/**
+ * given log(a) and log(b), return log(a - b) a>b
+ *
+ */
+
+double log_subtract(double log_a, double log_b)
+{
+    if (log_a < log_b) return -1000.0;
+
+    double v;
+    v = log_a + log(1 - exp(log_b-log_a));
+    return v;
+}
+
+
 double randgamma(double rr) {
   double aa, bb, cc, dd;
   double uu, vv, ww, xx, yy, zz;
