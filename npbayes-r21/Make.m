3fffb6bcccf1d728f7b9dba8b0e19dfe51ba6902
mt@mtsheep <mtli@cmu.edu>
2016-04-19 00:54:43 -0400

    Update make

---
+++ npbayes-r21/Make.m
@@ -0,0 +1,28 @@
+bDebugMex = 0;
+
+cd distributions/ourf;
+try
+    if bDebugMex
+        mex -g hdp_ourf_iterate.c
+    else
+        mex hdp_ourf_iterate.c
+    end
+catch ME
+    cd ../..;
+    rethrow(ME);
+end
+cd ../../utilities;
+try
+    if bDebugMex
+        mex -g randgamma.c
+        mex -g randnumtable.c
+    else
+        mex randgamma.c
+        mex randnumtable.c
+    end
+catch ME
+    cd ..
+    rethrow(ME);
+end
+cd ..
+
