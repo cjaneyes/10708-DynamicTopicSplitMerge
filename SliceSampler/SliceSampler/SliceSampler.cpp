fb9f4c1c046a6b749ed72a8388191b8174fedaa8
mt@mtsheep <mtli@cmu.edu>
2016-04-23 00:09:18 -0400

    Slice sampler with C++ RNG

---
+++ SliceSampler/SliceSampler/SliceSampler.cpp
@@ -0,0 +1,105 @@
+#include <iostream>
+#include <cmath>
+#include <cstdlib>
+#include <vector>
+#include <numeric>
+#include <algorithm>
+#include <random>
+#include <cassert>
+#include <limits.h>
+#include <conio.h>
+
+using namespace std;
+
+mt19937_64 gen;
+
+inline double uniform(double a, double b)
+{
+	uniform_real_distribution<double> ug(a, b);
+	return ug(gen);
+}
+
+double slice_sampling(double(*f)(double), double lower, double upper, double sigma)
+{
+	double x, y = 0;
+	while(y == 0)
+	{
+		x = uniform(lower, upper);	
+		y = uniform(0, f(x));
+		cout << y << endl;
+	}
+	double x_l = (x - lower > sigma) ? x - sigma : lower;
+	double x_r = (lower - x > sigma) ? x + sigma : upper;
+	
+	for(int i= 0;i < 10;i++)
+	{
+
+		for (int i = 0; i < 10; i++)
+		{
+			while (f(x_l) >= y)
+			{
+				double new_l = x - (x - x_l) * 2;
+				if (new_l < lower)
+				{
+					x_l = lower;
+					break;
+				}
+				x_l = new_l;
+			}
+			while (f(x_r) >= y)
+			{
+				double new_r = (x_r - x) * 2 + x;
+				if (new_r > upper)
+				{
+					x_r = upper;
+					break;
+				}
+				x_r = new_r;
+			}
+		}
+		//cout << f(x_l) << " " << f(x_r) << " " << y << endl;
+		while(true)
+		{
+			double new_x = uniform(x_l, x_r);
+			if(f(new_x) > y)
+			{
+				x = new_x;
+				break;
+			}
+		}
+		y = uniform(0, f(x));	
+		x_l = (x - lower > sigma) ? x - sigma : lower;
+		x_r = (lower - x > sigma) ? x + sigma : upper;	
+	}
+	return x;
+}
+
+double f(double x)
+{
+	return exp((-0.5) * x * x);
+}
+
+int main()
+{
+
+	assert(gen.min() == 0);
+	int n = 10000;
+	vector<double> sample(n);
+	for(int i = 0;i < n;i++)
+	{
+		sample[i] = slice_sampling(f, -1e2, 1e2, 100);
+		cout << sample[i] << endl;
+	}
+
+	auto &v = sample;
+	double sum = accumulate(v.begin(), v.end(), 0.0);
+	double mean = sum / v.size();
+
+	vector<double> diff(v.size());
+	transform(v.begin(), v.end(), diff.begin(), [mean](double x) { return x - mean; });
+	double sq_sum = std::inner_product(diff.begin(), diff.end(), diff.begin(), 0.0);
+	double stdev = std::sqrt(sq_sum / v.size());
+	cout << "mean: " << mean << ", stdev: " << stdev << endl;
+	//_getch();
+	return 0;
+}
---
+++ SliceSampler/SliceSampler/slice_sampling.cpp
@@ -0,0 +1,86 @@
+#include <iostream>
+#include <math.h>
+#include <time.h>
+#include <float.h>
+#include <stdlib.h>
+using namespace std;
+
+double uniform(double lower, double upper)
+{
+	double x = rand();
+	x = x * ((upper - lower) / RAND_MAX) + lower;
+	return x;
+}
+
+double slice_sampling(double (*f)(double), double lower, double upper, double sigma)
+{
+	double x, y = 0;
+	while(y == 0)
+	{
+		x = uniform(lower, upper);	
+		y = uniform(0, f(x));
+		cout << y << endl;
+	}
+	double x_l = (x - lower > sigma) ? x - sigma : upper;
+	double x_r = (lower - x > sigma) ? x + sigma : upper;
+	
+	for(int i= 0;i < 10;i++)
+	{
+		while (true)
+		{
+			//cout << f(upper) << endl;
+			if (f(x_r) >= y)
+			{
+				double new_r = (x_l - x) * 2 + x;
+				x_r = (new_r <= upper) ? new_r : upper;
+			}
+			else if (f(x_l) >= y)
+			{
+				double new_l = x - (x - x_l) * 2;
+				x_l = (new_l >= lower) ? new_l : lower;
+			}
+			else break;
+		}
+		//cout << f(x_l) << " " << f(x_r) << " " << y << endl;
+		while(true)
+		{
+			double new_x = uniform(x_l, x_r);
+			if(f(new_x) > y)
+			{
+				x = new_x;
+				break;
+			}
+		}
+		y = uniform(0, f(x));	
+		x_l = (x - lower > sigma) ? x - sigma : upper;
+		x_r = (lower - x > sigma) ? x + sigma : upper;	
+	}
+	return x;
+}
+
+double f(double x)
+{
+	return exp((-0.5) * x * x);
+}
+
+int main()
+{
+	double sample[100];
+	//srand (time(NULL));
+	cout << RAND_MAX << endl;
+	for(int i = 0;i < 100;i++)
+	{
+		sample[i] = slice_sampling(f, (-1) * (double) RAND_MAX / 10000, (double) RAND_MAX/ 10000, 100);
+		cout << sample[i] << endl;
+	}
+
+	double avg = 0, var = 0;
+	for(int i = 0;i < 100;i++)
+		avg += sample[i];
+	avg /= 100;
+	for(int i = 0;i < 100;i++)
+		var += (sample[i] - avg) * (sample[i] - avg);
+	var /= 100;
+
+	cout << "Mean: " << avg << ", Variance: " << var << endl;
+}
